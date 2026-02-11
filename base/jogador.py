721f50b34035920e2e37100b2fc336e8d44a7632
kennedy1907big <kfsilva1905@gmail.com>
2026-02-11 11:32:15 +0000

    Adicionando jogador

---
+++ base/jogador.py
@@ -0,0 +1,7 @@
+class Base_jogador:
+    def __init__(self, vida, defesa, ataque, esquiva):
+        self.vida = vida
+        self.defesa = defesa
+        self.ataque = ataque
+        self.esquiva = esquiva
+        pass
